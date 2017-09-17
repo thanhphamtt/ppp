d850a5b731debb8c3e5842fe28c49a2cfe44cf80
thanhphamtt <thanhpham1479@gmail.com>
2017-09-17 11:03:23 +0700

    update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
-
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    $http_origin = $_SERVER['HTTP_ORIGIN'];
+} else {
+    $http_origin = "http://localhost:3000";
+}
+header("Access-Control-Allow-Origin: $http_origin");
+//header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
