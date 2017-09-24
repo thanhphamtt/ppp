e4dcad46309cbdcc238e3f264037c63969991e5f
datvithanh98 <datvithanh98@gmail.com>
2017-09-24 10:54:07 +0700

    postlike_table

---
+++ database/migrations/2017_09_24_032701_create_like_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('postLikes', function (Blueprint $postLike) {
+            $postLike->increments('id');
+            $postLike->integer('user_id')->unsigned()->index();
+            $postLike->integer('post_id')->unsigned()->index();
+            $postLike->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
